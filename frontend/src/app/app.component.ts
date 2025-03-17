22f3c69545d8a0369b0bfa9284340d47a9077a6b
Colby Leed <colbyleed@yahoo.com>
2025-03-17 14:01:56 -0700

    improved header and body for SPA

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +13,11 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     RouterOutlet,
     HeroComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -13,5 +13,21 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  activeSection: string = 'hero';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = ['hero', 'about', 'projects', 'contact'];
+
+    for (let section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
+          this.activeSection = section;
+        }
+      }
+    }
+  }
 
 }
