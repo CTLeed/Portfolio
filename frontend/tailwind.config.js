da3618975ee5e8715d182e1a599ba3063a09356b
Colby Leed <colbyleed@yahoo.com>
2025-03-19 11:56:14 -0700

    improved background and customized theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
     './src/**/*.{html, ts}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'bg-blue': "#042db1",
+      'bg-black': "#030712",
+    },
     extend: {},
   },
   plugins: [],
 }
-
