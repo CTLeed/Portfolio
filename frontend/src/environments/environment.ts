b97d2f8bda5f5b30e5fca15e314d0530f64ed605
Colby Leed <colbyleed@yahoo.com>
2025-04-02 00:30:39 -0700

    combined environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api', // Local API URL for development
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api', // Local API URL for development
+  production: true,
+  apiUrl: 'https://springboot-backend-1-0.onrender.com/api', // Production API URL
+  // Add any other environment-specific variables here
 };
