45bb633984f02d83645f22e6112f5de2428343c8
Colby Leed <colbyleed@yahoo.com>
2025-04-01 23:52:28 -0700

    updated for static deployment

--- frontend/dist/frontend/server/chunk-DMI2NTOF.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-5XUXGTUW.mjs";var t=[{path:"",loadComponent:()=>import("./chunk-YZUKAIQO.mjs").then(o=>o.AdminDashboardComponent)}];export{t as ADMIN_ROUTES};
--- frontend/dist/frontend/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-K25NF2WL.mjs";import"./chunk-PLW5CRZ6.mjs";import"./chunk-TULFXKQA.mjs";import"./chunk-Z2MNXSGX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
