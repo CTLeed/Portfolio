27dfb051b149767a39c66b0952806c5294bbbe3a
Colby Leed <colbyleed@yahoo.com>
2025-07-18 12:57:21 -0700

    Improved authorization verification

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './services/auth.interceptor';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideHttpClient(
+      withFetch(), 
+      withInterceptors([authInterceptor])
+    ), 
+    provideAnimations()
+  ]
 };
